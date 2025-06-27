405735b940f4afa1399a26929fea56ccbad56e40
ychartois <ychartois@gmail.com>
2025-06-27 16:29:41 +0100

    initial commit of My Pizza App with basic HTML structure and React setup

---
+++ MyApp/src/App.js
@@ -0,0 +1,9 @@
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "My Pizza App"),
+  ]);
+}
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
